494fd09f21b96ace522cf2547d2cf713cdc51288
askec1705 <ahmetselimkaya1234@gmail.com>
2019-11-06 13:09:08 +0300

    Add project files.

---
+++ StringMetotlar/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace StringMetotlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTRIM = new System.Windows.Forms.Button();
+            this.btnTOCHARARRAY = new System.Windows.Forms.Button();
+            this.btnSUBSTRING = new System.Windows.Forms.Button();
+            this.btnTOLOWERTOUPPER = new System.Windows.Forms.Button();
+            this.btnINSERT = new System.Windows.Forms.Button();
+            this.btnSPLIT = new System.Windows.Forms.Button();
+            this.btnREPLACE = new System.Windows.Forms.Button();
+            this.btnREMOVE = new System.Windows.Forms.Button();
+            this.btnLASTINDEXOF = new System.Windows.Forms.Button();
+            this.btnINDEXOF = new System.Windows.Forms.Button();
+            this.btnENDSWITH = new System.Windows.Forms.Button();
+            this.btnSTARTSWITH = new System.Windows.Forms.Button();
+            this.btnCONTAINS = new System.Windows.Forms.Button();
+            this.txtGirisAlani = new System.Windows.Forms.TextBox();
+            this.btnCOMPARETO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTRIM
+            // 
+            this.btnTRIM.Location = new System.Drawing.Point(12, 577);
+            this.btnTRIM.Name = "btnTRIM";
+            this.btnTRIM.Size = new System.Drawing.Size(284, 35);
+            this.btnTRIM.TabIndex = 44;
+            this.btnTRIM.Text = "TRIM";
+            this.btnTRIM.UseVisualStyleBackColor = true;
+            this.btnTRIM.Click += new System.EventHandler(this.BtnTRIM_Click);
+            // 
+            // btnTOCHARARRAY
+            // 
+            this.btnTOCHARARRAY.Location = new System.Drawing.Point(12, 536);
+            this.btnTOCHARARRAY.Name = "btnTOCHARARRAY";
+            this.btnTOCHARARRAY.Size = new System.Drawing.Size(284, 35);
+            this.btnTOCHARARRAY.TabIndex = 43;
+            this.btnTOCHARARRAY.Text = "TOCHARARRAY";
+            this.btnTOCHARARRAY.UseVisualStyleBackColor = true;
+            this.btnTOCHARARRAY.Click += new System.EventHandler(this.BtnTOCHARARRAY_Click);
+            // 
+            // btnSUBSTRING
+            // 
+            this.btnSUBSTRING.Location = new System.Drawing.Point(12, 495);
+            this.btnSUBSTRING.Name = "btnSUBSTRING";
+            this.btnSUBSTRING.Size = new System.Drawing.Size(284, 35);
+            this.btnSUBSTRING.TabIndex = 42;
+            this.btnSUBSTRING.Text = "SUBSTRING";
+            this.btnSUBSTRING.UseVisualStyleBackColor = true;
+            this.btnSUBSTRING.Click += new System.EventHandler(this.BtnSUBSTRING_Click);
+            // 
+            // btnTOLOWERTOUPPER
+            // 
+            this.btnTOLOWERTOUPPER.Location = new System.Drawing.Point(12, 454);
+            this.btnTOLOWERTOUPPER.Name = "btnTOLOWERTOUPPER";
+            this.btnTOLOWERTOUPPER.Size = new System.Drawing.Size(284, 35);
+            this.btnTOLOWERTOUPPER.TabIndex = 41;
+            this.btnTOLOWERTOUPPER.Text = "TOLOWER - TOUPPER";
+            this.btnTOLOWERTOUPPER.UseVisualStyleBackColor = true;
+            this.btnTOLOWERTOUPPER.Click += new System.EventHandler(this.BtnTOLOWERTOUPPER_Click);
+            // 
+            // btnINSERT
+            // 
+            this.btnINSERT.Location = new System.Drawing.Point(12, 413);
+            this.btnINSERT.Name = "btnINSERT";
+            this.btnINSERT.Size = new System.Drawing.Size(284, 35);
+            this.btnINSERT.TabIndex = 40;
+            this.btnINSERT.Text = "INSERT";
+            this.btnINSERT.UseVisualStyleBackColor = true;
+            this.btnINSERT.Click += new System.EventHandler(this.BtnINSERT_Click);
+            // 
+            // btnSPLIT
+            // 
+            this.btnSPLIT.Location = new System.Drawing.Point(12, 372);
+            this.btnSPLIT.Name = "btnSPLIT";
+            this.btnSPLIT.Size = new System.Drawing.Size(284, 35);
+            this.btnSPLIT.TabIndex = 39;
+            this.btnSPLIT.Text = "SPLIT";
+            this.btnSPLIT.UseVisualStyleBackColor = true;
+            this.btnSPLIT.Click += new System.EventHandler(this.BtnSPLIT_Click);
+            // 
+            // btnREPLACE
+            // 
+            this.btnREPLACE.Location = new System.Drawing.Point(12, 331);
+            this.btnREPLACE.Name = "btnREPLACE";
+            this.btnREPLACE.Size = new System.Drawing.Size(284, 35);
+            this.btnREPLACE.TabIndex = 38;
+            this.btnREPLACE.Text = "REPLACE";
+            this.btnREPLACE.UseVisualStyleBackColor = true;
+            this.btnREPLACE.Click += new System.EventHandler(this.BtnREPLACE_Click);
+            // 
+            // btnREMOVE
+            // 
+            this.btnREMOVE.Location = new System.Drawing.Point(12, 290);
+            this.btnREMOVE.Name = "btnREMOVE";
+            this.btnREMOVE.Size = new System.Drawing.Size(284, 35);
+            this.btnREMOVE.TabIndex = 37;
+            this.btnREMOVE.Text = "REMOVE";
+            this.btnREMOVE.UseVisualStyleBackColor = true;
+            this.btnREMOVE.Click += new System.EventHandler(this.BtnREMOVE_Click);
+            // 
+            // btnLASTINDEXOF
+            // 
+            this.btnLASTINDEXOF.Location = new System.Drawing.Point(12, 249);
+            this.btnLASTINDEXOF.Name = "btnLASTINDEXOF";
+            this.btnLASTINDEXOF.Size = new System.Drawing.Size(284, 35);
+            this.btnLASTINDEXOF.TabIndex = 36;
+            this.btnLASTINDEXOF.Text = "LASTINDEXOF";
+            this.btnLASTINDEXOF.UseVisualStyleBackColor = true;
+            this.btnLASTINDEXOF.Click += new System.EventHandler(this.BtnLASTINDEXOF_Click);
+            // 
+            // btnINDEXOF
+            // 
+            this.btnINDEXOF.Location = new System.Drawing.Point(12, 208);
+            this.btnINDEXOF.Name = "btnINDEXOF";
+            this.btnINDEXOF.Size = new System.Drawing.Size(284, 35);
+            this.btnINDEXOF.TabIndex = 35;
+            this.btnINDEXOF.Text = "INDEXOF";
+            this.btnINDEXOF.UseVisualStyleBackColor = true;
+            this.btnINDEXOF.Click += new System.EventHandler(this.BtnINDEXOF_Click);
+            // 
+            // btnENDSWITH
+            // 
+            this.btnENDSWITH.Location = new System.Drawing.Point(12, 167);
+            this.btnENDSWITH.Name = "btnENDSWITH";
+            this.btnENDSWITH.Size = new System.Drawing.Size(284, 35);
+            this.btnENDSWITH.TabIndex = 34;
+            this.btnENDSWITH.Text = "ENDSWITH";
+            this.btnENDSWITH.UseVisualStyleBackColor = true;
+            this.btnENDSWITH.Click += new System.EventHandler(this.BtnENDSWITH_Click);
+            // 
+            // btnSTARTSWITH
+            // 
+            this.btnSTARTSWITH.Location = new System.Drawing.Point(12, 126);
+            this.btnSTARTSWITH.Name = "btnSTARTSWITH";
+            this.btnSTARTSWITH.Size = new System.Drawing.Size(284, 35);
+            this.btnSTARTSWITH.TabIndex = 33;
+            this.btnSTARTSWITH.Text = "STARTSWITH";
+            this.btnSTARTSWITH.UseVisualStyleBackColor = true;
+            this.btnSTARTSWITH.Click += new System.EventHandler(this.BtnSTARTSWITH_Click);
+            // 
+            // btnCONTAINS
+            // 
+            this.btnCONTAINS.Location = new System.Drawing.Point(12, 85);
+            this.btnCONTAINS.Name = "btnCONTAINS";
+            this.btnCONTAINS.Size = new System.Drawing.Size(284, 35);
+            this.btnCONTAINS.TabIndex = 32;
+            this.btnCONTAINS.Text = "CONTAINS";
+            this.btnCONTAINS.UseVisualStyleBackColor = true;
+            this.btnCONTAINS.Click += new System.EventHandler(this.BtnCONTAINS_Click);
+            // 
+            // txtGirisAlani
+            // 
+            this.txtGirisAlani.Location = new System.Drawing.Point(12, 12);
+            this.txtGirisAlani.Name = "txtGirisAlani";
+            this.txtGirisAlani.Size = new System.Drawing.Size(284, 20);
+            this.txtGirisAlani.TabIndex = 31;
+            // 
+            // btnCOMPARETO
+            // 
+            this.btnCOMPARETO.Location = new System.Drawing.Point(12, 44);
+            this.btnCOMPARETO.Name = "btnCOMPARETO";
+            this.btnCOMPARETO.Size = new System.Drawing.Size(284, 35);
+            this.btnCOMPARETO.TabIndex = 30;
+            this.btnCOMPARETO.Text = "COMPARE TO";
+            this.btnCOMPARETO.UseVisualStyleBackColor = true;
+            this.btnCOMPARETO.Click += new System.EventHandler(this.BtnCOMPARETO_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 625);
+            this.Controls.Add(this.btnTRIM);
+            this.Controls.Add(this.btnTOCHARARRAY);
+            this.Controls.Add(this.btnSUBSTRING);
+            this.Controls.Add(this.btnTOLOWERTOUPPER);
+            this.Controls.Add(this.btnINSERT);
+            this.Controls.Add(this.btnSPLIT);
+            this.Controls.Add(this.btnREPLACE);
+            this.Controls.Add(this.btnREMOVE);
+            this.Controls.Add(this.btnLASTINDEXOF);
+            this.Controls.Add(this.btnINDEXOF);
+            this.Controls.Add(this.btnENDSWITH);
+            this.Controls.Add(this.btnSTARTSWITH);
+            this.Controls.Add(this.btnCONTAINS);
+            this.Controls.Add(this.txtGirisAlani);
+            this.Controls.Add(this.btnCOMPARETO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTRIM;
+        private System.Windows.Forms.Button btnTOCHARARRAY;
+        private System.Windows.Forms.Button btnSUBSTRING;
+        private System.Windows.Forms.Button btnTOLOWERTOUPPER;
+        private System.Windows.Forms.Button btnINSERT;
+        private System.Windows.Forms.Button btnSPLIT;
+        private System.Windows.Forms.Button btnREPLACE;
+        private System.Windows.Forms.Button btnREMOVE;
+        private System.Windows.Forms.Button btnLASTINDEXOF;
+        private System.Windows.Forms.Button btnINDEXOF;
+        private System.Windows.Forms.Button btnENDSWITH;
+        private System.Windows.Forms.Button btnSTARTSWITH;
+        private System.Windows.Forms.Button btnCONTAINS;
+        private System.Windows.Forms.TextBox txtGirisAlani;
+        private System.Windows.Forms.Button btnCOMPARETO;
+    }
+}
+
---
+++ StringMetotlar/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringMetotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ornekMetin = "";
+        #region Örnek 1
+        private void BtnCOMPARETO_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            int result = ornekMetin.CompareTo("araba");
+            string mesaj = "";
+            switch (result)
+            {
+                case -1:
+                    mesaj = "Kelime, diğer kelime ile eşleşmemektedir, eksik karakter mevcut.";
+                    break;
+                case 0:
+                    mesaj = "Kelime, diğer kelime ile eşleşmektedir.";
+                    break;
+                case 1:
+                    mesaj = "Kelime, diğer kelime ile eşleşmemektedir, fazla karakter mevcut.";
+                    break;
+                default:
+                    break;
+            }
+            MessageBox.Show(mesaj);
+        }
+        #endregion
+
+        #region Örnek 2
+        private void BtnCONTAINS_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            bool result = ornekMetin.Contains("adam");
+            MessageBox.Show($"Aradığınız kelime metin içerisinde geçme{(result ? "" : "me")}ktedir");
+        }
+        #endregion
+
+        #region Örnek 3
+        private void BtnSTARTSWITH_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            bool result = ornekMetin.StartsWith("bil");
+
+            MessageBox.Show($"Metin parametrede gönderdiğiniz anahtar kelime ile başlama{(result ? "" : "")}ktadır");
+        }
+        #endregion
+
+        #region Örnek 4
+        private void BtnENDSWITH_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            bool result = ornekMetin.EndsWith("dam");
+
+            MessageBox.Show($"Kelime parametrede gönderdiğiniz değer ile bitme{(result ? "" : "me")}ktedir");
+        }
+        #endregion
+
+        #region Örnek 5
+        private void BtnINDEXOF_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            int index = ornekMetin.IndexOf('@');
+
+            MessageBox.Show(index > -1 ? "Aradığınız karakterin metin içerisindeki index değeri : " + index : "Metin içerisinde aradığınız parametre yer almamaktadır");
+        }
+        #endregion
+
+        #region Örnek 6
+        private void BtnLASTINDEXOF_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            int index = ornekMetin.LastIndexOf('a');
+
+            MessageBox.Show(index > -1 ? "Aradığınız karakterin metin içerisindeki index değeri : " + index : "Metin içerisinde aradığınız parametre yer almamaktadır");
+        }
+        #endregion
+
+        #region Örnek 7
+        private void BtnREMOVE_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+
+            //ornekMetin = ornekMetin.Remove(5); verdiğiniz index değeri dahil olmak üzere kalan tüm karakterleri siler.
+            ornekMetin = ornekMetin.Remove(3, 2);
+            //1. Parametre, başlangıç index değeri.
+            //2. Parametre, silinecek eleman sayısı.
+            MessageBox.Show(ornekMetin);
+        }
+        #endregion
+
+        #region Örnek 8
+        private void BtnREPLACE_Click(object sender, EventArgs e)
+        {
+            //ornekMetin = txtGirisAlani.Text;
+            //ornekMetin = ornekMetin.Replace("adam", "hatun");
+            //MessageBox.Show(ornekMetin);
+
+            ornekMetin = txtGirisAlani.Text;
+            ornekMetin = ornekMetin.Replace("ı", "i").Replace("ç", "c").Replace("ü", "u").Replace("ö", "o").Replace("ş", "s").Replace("ğ", "g");
+            //ornekMetin = ornekMetin.Replace("ı", "i");
+            //ornekMetin = ornekMetin.Replace("ç", "c");
+            //ornekMetin = ornekMetin.Replace("ü", "u");
+            //ornekMetin = ornekMetin.Replace("ö", "o");
+            //ornekMetin = ornekMetin.Replace("ş", "s");
+            //ornekMetin = ornekMetin.Replace("ğ", "g");
+            MessageBox.Show(ornekMetin);
+        }
+        #endregion
+
+        #region Örnek 9
+        private void BtnSPLIT_Click(object sender, EventArgs e)
+        {
+            //string[] mailAdresler = txtGirisAlani.Text.Split(';');
+            //foreach (string mail in mailAdresler)
+            //{
+            //    MessageBox.Show(mail);
+            //}
+
+            string[] veriler = txtGirisAlani.Text.Split(';', ',', '+', '.');
+            foreach (string veri in veriler)
+            {
+                MessageBox.Show(veri);
+            }
+        }
+        #endregion
+
+        #region Örnek 10
+        private void BtnINSERT_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            ornekMetin = ornekMetin.Insert(5, "cuk");
+            MessageBox.Show(ornekMetin);
+        }
+        #endregion
+
+        #region Örnek 11
+        private void BtnTOLOWERTOUPPER_Click(object sender, EventArgs e)
+        {
+            this.Text = txtGirisAlani.Text.ToLower();
+            MessageBox.Show(txtGirisAlani.Text.ToUpper());
+        }
+        #endregion
+
+        #region Örnek 12
+        private void BtnSUBSTRING_Click(object sender, EventArgs e)
+        {
+            //Remove (5) => "bilge adam" => "bilge"
+            //Substring (5) => "bilge adam" => " adam"
+
+            ornekMetin = txtGirisAlani.Text;
+            //ornekMetin = ornekMetin.Substring(5);
+
+            //Remove (3, 2) => "bilge adam" => "bil adam"
+            //Substring (3, 2) => "bilge adam" => "ge"
+            ornekMetin = ornekMetin.Substring(3, 2);
+
+            MessageBox.Show(ornekMetin);
+        }
+        #endregion
+
+        #region Örnek 13
+        private void BtnTOCHARARRAY_Click(object sender, EventArgs e)
+        {
+            //ornekMetin = txtGirisAlani.Text;
+            //char[] karakterler = ornekMetin.ToCharArray();
+
+            ornekMetin = txtGirisAlani.Text;
+            char[] karakterler = ornekMetin.ToCharArray();
+            int toplam = 0;
+            foreach (char karakter in karakterler)
+            {
+                toplam += karakter;
+            }
+            MessageBox.Show(toplam.ToString());
+        }
+        #endregion
+
+        #region Örnek 14
+        private void BtnTRIM_Click(object sender, EventArgs e)
+        {
+            ornekMetin = txtGirisAlani.Text;
+            int l1 = ornekMetin.Length;
+            ornekMetin = ornekMetin.Trim();
+            int l2 = ornekMetin.Length;
+
+            MessageBox.Show($"Metin boşluklar dahil toplamda {l1} karakterden oluşmaktadır!\nMetin boşluklar silinerek toplamda {l2} karakterden oluşmaktadır");
+
+            //"".TrimEnd() => metnin sonundaki boşlukları siler
+            //"".TrimStart() => metnin başındaki boşlukları siler
+        } 
+        #endregion
+    }
+}
